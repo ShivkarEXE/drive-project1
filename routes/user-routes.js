0b84be89a5b4dd03b9805daf8dade5ec6c23a2d4
ShivkarEXE <addharashiokar@gmail.com>
2026-02-01 16:44:41 +0530

    eod1

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,8 +1,13 @@
-const express = require('express'); 
+const express = require("express");
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-    res.send('User test routine');
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("Registration successful");
 });
 
 module.exports = router;
